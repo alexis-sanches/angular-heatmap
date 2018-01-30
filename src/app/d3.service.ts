3928f652844b2d2474cbc44a3bf51c3ad7b19ab9
Alexander Sannikov <a.sannikov@netrika.ru>
2018-01-30 15:40:32 +0300

    feature-1 Render map and color scale

--- src/app/d3.service.ts
+++ src/app/d3.service.ts
@@ -1,22 +1,61 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as d3 from 'd3';
+import {IHeatmapOptions} from './heatmap/heatmap.component';
 
 @Injectable()
 export class D3Service {
-  public static getNewScale(length: number, colorFrom: string, colorTo: string) {
-    const body = d3.select(`body`);
-    const color = d3.scaleLinear().domain([1, length])
-      .interpolate(d3.interpolateHcl)
-      .range([d3.rgb('#007AFF'), d3.rgb('#FFF500')]);
+  private data: IHeatmapOptions[];
+  private svg: any;
 
-    for (let i = 0; i < length; i++) {
-      body.append('div').attr('style', function (d) {
-        return 'background-color: ' + color(i);
-      });
+  public static createScale(colors: string[], length: number = 10): any {
+    return d3.scaleLinear<string>()
+      .domain([1, length])
+      .range(colors);
+  }
+
+  public appendSvg(element: any, svg: string): void {
+    this.svg = d3.select(element)
+      .html(svg)
+      .select(`svg`);
+  }
+
+  public appendScale(range: any): void {
+    const g = this.svg.append(`g`);
+
+    g.append(`text`)
+      .attr(`fill`, `#000`)
+      .attr(`y`, 40)
+      .attr(`x`, 180)
+      .text(`min`);
+
+    g.append(`text`)
+      .attr(`fill`, `#000`)
+      .attr(`y`, 40)
+      .attr(`x`, 280)
+      .text(`max`);
+
+    for (let i = 0; i < 10; i += 1) {
+      g.append(`rect`)
+        .attr(`fill`, range(i))
+        .attr(`width`, 10)
+        .attr(`height`, 20)
+        .attr(`x`, 200 + 10 * i);
     }
   }
 
-  public static appendSvg(id: string, svg: string) {
-    d3.select(`#${id}`).append(`div`).html(svg);
+  public createLegend() {
+
+  }
+
+  public createMap() {
+
+  }
+
+  public appendLegend() {
+
+  }
+
+  public appendMap() {
+
   }
 }
--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output} from '@angular/core';
+import {D3Service} from '../d3.service';
 
 export interface IHeatmapOptions {
   id: string;
@@ -11,12 +12,29 @@ export interface IHeatmapOptions {
   templateUrl: './heatmap.component.html',
   styleUrls: ['./heatmap.component.less']
 })
-export class HeatmapComponent implements OnInit {
+export class HeatmapComponent {
+  private _svg: string;
+  private _colors: string[];
   private data: IHeatmapOptions[];
+  private scale;
 
-  constructor() { }
-
-  ngOnInit() {
+  @Input()
+  set svg(svg: string) {
+    if (svg) {
+      this.d3.appendSvg(this.el.nativeElement, svg);
+    }
   }
 
+  @Input()
+  set colors(colors: string[]) {
+    if (colors) {
+      this.d3.appendScale(D3Service.createScale(colors));
+    }
+  }
+
+  @Output() select: EventEmitter<IHeatmapOptions> = new EventEmitter();
+
+  @Output() sort: EventEmitter<string> = new EventEmitter();
+
+  constructor(private d3: D3Service, private el: ElementRef) {}
 }
