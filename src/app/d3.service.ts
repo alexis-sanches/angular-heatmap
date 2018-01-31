cc78e8b7cd05c79584ad14160f2e15719fa70108
Alexander Sannikov <a.sannikov@netrika.ru>
2018-01-31 18:10:26 +0300

    feature-2 Legend

--- src/app/d3.service.ts
+++ src/app/d3.service.ts
@@ -1,61 +1,102 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import * as d3 from 'd3';
 import {IHeatmapOptions} from './heatmap/heatmap.component';
 
 @Injectable()
 export class D3Service {
-  private data: IHeatmapOptions[];
-  private svg: any;
+    private element: any;
+    private svg: any;
+    private scale: any;
+    public selectDetector: EventEmitter<IHeatmapOptions> = new EventEmitter();
 
-  public static createScale(colors: string[], length: number = 10): any {
-    return d3.scaleLinear<string>()
-      .domain([1, length])
-      .range(colors);
-  }
+    /**
+     *
+     * @param options
+     * @param element
+     * @param {number} length
+     */
 
-  public appendSvg(element: any, svg: string): void {
-    this.svg = d3.select(element)
-      .html(svg)
-      .select(`svg`);
-  }
+    public init(options: any, element: any, length: number = 10) {
+        this.element = d3.select(element);
+        this.svg = this.element.append(`div`).html(options.svg).select(`svg`);
 
-  public appendScale(range: any): void {
-    const g = this.svg.append(`g`);
-
-    g.append(`text`)
-      .attr(`fill`, `#000`)
-      .attr(`y`, 40)
-      .attr(`x`, 180)
-      .text(`min`);
-
-    g.append(`text`)
-      .attr(`fill`, `#000`)
-      .attr(`y`, 40)
-      .attr(`x`, 280)
-      .text(`max`);
-
-    for (let i = 0; i < 10; i += 1) {
-      g.append(`rect`)
-        .attr(`fill`, range(i))
-        .attr(`width`, 10)
-        .attr(`height`, 20)
-        .attr(`x`, 200 + 10 * i);
+        this.createScale(options.colors, length);
+        this.setData(options.data);
     }
-  }
 
-  public createLegend() {
+    /**
+     *
+     * @param {string[]} colors
+     * @param {number} length
+     * @return {any}
+     */
 
-  }
+    public createScale(colors: string[], length: number = 10): any {
+        const domain = [1];
 
-  public createMap() {
+        for (let i = 0; i < colors.length - 1; i++) {
+            domain.push((i + 1) * length / (colors.length - 1));
+        }
 
-  }
+        this.scale = d3.scaleLinear<string>()
+            .domain(domain)
+            .range(colors);
 
-  public appendLegend() {
+        this.appendScale();
+    }
 
-  }
+    /**
+     *
+     */
 
-  public appendMap() {
+    private appendScale(): void {
+        const g = this.svg.append(`g`);
 
-  }
+        g.append(`text`)
+            .attr(`y`, 20)
+            .attr(`x`, 200)
+            .text(`МИН`);
+
+        g.append(`text`)
+            .attr(`y`, 20)
+            .attr(`x`, 278)
+            .text(`МАКС`);
+
+        for (let i = 0; i < 10; i += 1) {
+            g.append(`rect`)
+                .attr(`fill`, this.scale(i))
+                .attr(`width`, 10)
+                .attr(`height`, 10)
+                .attr(`x`, 200 + 10 * i);
+        }
+    }
+
+    /**
+     *
+     * @param {IHeatmapOptions[]} data
+     * @param {number} length
+     */
+
+    public setData(data: IHeatmapOptions[], length: number = 10) {
+        const table = this.element.append(`table`).attr(`class`, `legend`);
+        const values = data.map((it) => it.value);
+        const th = table.append(`tr`);
+
+        th.append(`th`).text(`Количество`);
+        th.append(`th`).text(`Название`);
+
+        for (let i = 0; i < data.length; i++) {
+            const tr = table.append(`tr`);
+            const j = (data[i].value - Math.min(...values)) / (Math.max(...values) - Math.min(...values)) * length;
+
+            this.svg.select(`#${data[i].id}`)
+                .attr(`style`, `fill: ${this.scale(j)}`)
+                .on(`click`, () => {
+                    this.selectDetector.next(data[i]);
+                });
+
+            tr.append(`td`).text(data[i].value).attr(`style`, `background-color: ${this.scale(j)}`);
+            tr.append(`td`).text(data[i].title);
+        }
+    }
 }
--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -2,39 +2,36 @@ import {Component, ElementRef, EventEmitter, Input, Output} from '@angular/core'
 import {D3Service} from '../d3.service';
 
 export interface IHeatmapOptions {
-  id: string;
-  title: string;
-  value: number;
+    id: string;
+    title: string;
+    value: number;
 }
 
 @Component({
-  selector: 'app-heatmap',
-  templateUrl: './heatmap.component.html',
-  styleUrls: ['./heatmap.component.less']
+    selector: 'app-heatmap',
+    templateUrl: './heatmap.component.html',
+    styleUrls: ['./heatmap.component.less']
 })
 export class HeatmapComponent {
-  private _svg: string;
-  private _colors: string[];
-  private data: IHeatmapOptions[];
-  private scale;
+    private _svg: string;
+    private _colors: string[];
+    private _data: IHeatmapOptions[];
+    private scale;
 
-  @Input()
-  set svg(svg: string) {
-    if (svg) {
-      this.d3.appendSvg(this.el.nativeElement, svg);
+    @Input()
+    set options(options: any) {
+        if (options) {
+            this.d3.init(options, this.el.nativeElement);
+        }
     }
-  }
 
-  @Input()
-  set colors(colors: string[]) {
-    if (colors) {
-      this.d3.appendScale(D3Service.createScale(colors));
+    @Output() select: EventEmitter<IHeatmapOptions> = new EventEmitter();
+
+    @Output() sort: EventEmitter<string> = new EventEmitter();
+
+    constructor(private d3: D3Service, private el: ElementRef) {
+        d3.selectDetector.subscribe((res) => {
+            this.select.emit(res);
+        });
     }
-  }
-
-  @Output() select: EventEmitter<IHeatmapOptions> = new EventEmitter();
-
-  @Output() sort: EventEmitter<string> = new EventEmitter();
-
-  constructor(private d3: D3Service, private el: ElementRef) {}
 }
